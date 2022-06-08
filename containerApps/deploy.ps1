57e307a44ab0f9206f255cf2a2d40b8f58e92814
Matt Leeke <maleekie@gmail.com>
2022-06-09 00:01:40 +0100

    Adding traffic generator and code to deploy multiple revisions

--- containerApps/deploy.ps1
+++ containerApps/deploy.ps1
@@ -7,3 +7,21 @@ az deployment group create `
   --name $deployName `
   --resource-group $resourceGroup `
   --template-file ./containerApp.bicep
+
+Write-Host "Adding revision 2 of application..." -ForegroundColor Yellow
+az containerapp update --name app-petsapp `
+  --resource-group rg-containerapp `
+  --image acrdemo49577068.azurecr.io/apps/containerapp:v2 `
+  --revision-suffix v2
+
+Write-Host "Reverting revision 1 to 100% of traffic..." -ForegroundColor Yellow
+az containerapp ingress traffic set --name app-petsapp `
+  --resource-group rg-containerapp `
+  --revision-weight app-petsapp--v1=100
+
+<#
+Write-Host "Split traffic across revisions..." -ForegroundColor Yellow
+az containerapp ingress traffic set --name app-petsapp `
+  --resource-group rg-containerapp `
+  --revision-weight app-petsapp--v1=95 app-petsapp--v2=5
+#>
